7ed357b06b17e9e0966eb1130debffc8de09cde3
Yosi Izaq <yosi.izaq@sap.com>
2022-06-19 16:34:04 +0300

    improve abort demo

--- abortControllerDemo.mjs
+++ abortControllerDemo.mjs
@@ -1,29 +1,32 @@
 import { setTimeout, setInterval, setImmediate} from 'timers/promises';
 
+/*
+long running promise 
+note use of options argument to pass the abort signal in
+also not check for aborted signal after firing the long time operation (simulated by setTimeout)
+ */
 const someLongRunningTask = async (delay, msg, options) => {
    const signal = {...options};
     console.log('got signal', signal);
-    if (signal.aborted === true) {
+   const res = await setTimeout(delay, msg);
+    console.log('got signal 2 ', JSON.stringfy(signal, null, 4));
+    if (signal.signal.aborted === true) {
         throw new Error('someLongRunningTask cancelled');
     }
-   const res = await setTimeout(delay, msg);
    console.log(`waited ${delay}, ${msg}`);
 };
 
-const timeout = new Promise( (res, rej) => {
-    setTimeout( () => reject(new Error('timeout expired')), 6000);
-});
-
-
 console.log('Cancel promise using AbortController');
+
+// straightforward, an AbortController for TO and Task promises
 const cancelTO = new AbortController();
 const cancelTask = new AbortController();
 
 async function timeout1() {
     try {
-        await setTimeout(1000, undefined, {signal: cancelTO.signal});
+        await setTimeout(1000, undefined, {signal: cancelTO.signal}); // note, setTimeout options argument passes cancelTO signal
         console.log('aborting task');
-        cancelTask.abort();
+        cancelTask.abort(); // timeout1 cancels task after 1 second
         
     } catch (e) {
         /* handle error */
@@ -32,10 +35,10 @@ async function timeout1() {
 
 async function task() {
     try {
-        await someLongRunningTask(5000,  'long running task finished', {signal: cancelTask.signal});
+        await someLongRunningTask(5000,  'long running task finished', {signal: cancelTask.signal}); //  note, setTimeout options argument passes cancelTask signal 
     }
     finally {
-        cancelTO.abort();
+        cancelTO.abort(); // task cancels timeout1 
     }
     }
 
